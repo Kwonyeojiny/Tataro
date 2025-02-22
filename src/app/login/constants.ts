bbbe2967d3738e439e4eb5232a0fcd0904818b1f
Seyoon Jeong <seyoonagain@gmail.com>
2025-02-22 20:18:54 +0900

    chore: 액세스 토큰 만료 시간 및 만료 전 재발급 타임을 상수로 분리

--- src/app/login/constants.ts
+++ src/app/login/constants.ts
@@ -3,3 +3,7 @@ import { OAuthProviderType } from './types';
 export const OAUTH_PROVIDERS: OAuthProviderType[] = ['kakao', 'naver'];
 
 export const OAUTH_PROVIDERS_KOR = { kakao: '카카오', naver: '네이버' };
+
+export const ACCESS_TOKEN_EXPIRY = 1000 * 60 * 60;
+
+export const TIME_BEFORE_EXPIRATION = 1000 * 60 * 5;
