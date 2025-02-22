11a5c5943dbe4f7c13695a1175cefd3d02734677
hatnim <dlgotsla3391@gmail.com>
2025-02-23 01:34:23 +0900

    feat: 챗목록, 리뷰목록, 리뷰 디테일에 카드 이미지 연결

--- src/components/reviews/reviewCard/index.tsx
+++ src/components/reviews/reviewCard/index.tsx
@@ -1,19 +1,26 @@
+import { useEffect, useState } from 'react';
 import Image from 'next/image';
 import { Eye } from 'lucide-react';
 
+import { getTarotChatHistory } from '@/api/tarotApi';
 import useScreenWidth from '@/hooks/useScreenWidth';
 import useLayerCardStore from '@/stores/layerCardStore';
+import { TarotCard } from '@/types/tarot';
 
 import { layerCard } from '@common/layerCard';
-
-import TheFool from '@images/TheFool.svg';
 
 import ReviewDetail from '../reviewDetail';
 
 import { ReviewCardProps } from '../types';
 
+type CardImage = {
+  url: string;
+  name: string;
+  direction: string;
+};
 const ReviewCard: React.FC<ReviewCardProps> = ({
   id,
+  room_id,
   title,
   content,
   user_nickname,
@@ -23,10 +30,33 @@ const ReviewCard: React.FC<ReviewCardProps> = ({
 }) => {
   const { hideLayerCard } = useLayerCardStore();
   const { isCustomWidth } = useScreenWidth(640);
+  const [cardImages, setCardImages] = useState<CardImage[]>([]);
+
+  useEffect(() => {
+    const fetchChatImages = async () => {
+      if (!room_id) {
+        return;
+      }
+      try {
+        const data = await getTarotChatHistory(room_id.toString());
+        const cardImagesData = data.chat_log.map((log: TarotCard) => ({
+          url: log.card_url,
+          name: log.card_name,
+          direction: log.card_direction,
+        }));
+        setCardImages(cardImagesData);
+      } catch (error) {
+        console.error('Failed to fetch chat history', error);
+      }
+    };
+    fetchChatImages();
+  }, [room_id]);
 
   const showLayerCard = () => {
     layerCard({
-      content: <ReviewDetail review_id={id} close={() => hideLayerCard()} />,
+      content: (
+        <ReviewDetail review_id={id} cardImages={cardImages} close={() => hideLayerCard()} />
+      ),
       size: 'max-w-5xl max-h-[768px]',
     });
   };
@@ -38,13 +68,28 @@ const ReviewCard: React.FC<ReviewCardProps> = ({
         className={`flex items-center w-full max-h-32 border border-purple bg-lightPink hover:-translate-y-0.5 hover:shadow-md transition-all duration-200 hover:cursor-pointer ${isCustomWidth ? 'gap-4 px-4 py-3' : 'gap-5 px-6 py-4 '}`}
         aria-label={`${title} 리뷰 상세보기`}
       >
-        <Image
-          src={TheFool}
-          alt="타로카드"
-          width={50}
-          className={`${isCustomWidth ? 'hidden' : 'block'}`}
-        />
-        <div className="flex flex-col justify-center w-full h-full text-purple">
+        <div
+          className={`relative flex items-center w-[82px] h-[94px] ${isCustomWidth ? 'hidden' : 'block'}`}
+        >
+          {cardImages.slice(0, 3).map((image, index) => (
+            <Image
+              key={index}
+              src={image.url}
+              alt={image.name}
+              width={50}
+              height={87}
+              className={`absolute
+                        ${index === 0 ? 'left-0' : index === 1 ? 'left-4' : 'left-8'}`}
+              style={{
+                zIndex: index,
+                width: 'auto',
+                height: '90%',
+                transform: image.direction === '역방향' ? 'rotate(180deg)' : 'none',
+              }}
+            />
+          ))}
+        </div>
+        <div className="flex flex-col flex-1 justify-center h-full text-purple">
           <div
             className={`flex items-center w-full ${isCustomWidth ? 'justify-between' : 'gap-5'}`}
           >
