a7e7ce6a2f7e27f3aecd0ded91878fa0cd74556f
Sangwoo Shin <ssangwoo96@gmail.com>
2025-02-23 06:05:12 +0900

    feat: notice 페이지 api 연결 완료

--- src/app/notice/[id]/page.tsx
+++ src/app/notice/[id]/page.tsx
@@ -9,53 +9,13 @@ type Props = {
 const NoticeDetailPage = async ({ params }: Props) => {
   const noticeId = params.id;
 
-  // 임시 데이터 (실제로는 API에서 가져와야 함)
-  const data = {
-    page: 1,
-    size: 10,
-    total_pages: 1,
-    total_count: 4,
-    results: [
-      {
-        id: 1,
-        created_at: '2025-02-20T19:11:46.934281+09:00',
-        updated_at: '2025-02-20T19:11:46.934312+09:00',
-        title: '첫 공지',
-        content: '테스트 중입니다',
-        img_url:
-          'https://i.namu.wiki/i/Bbq0E9hXYyrXbL4TnIE__vtQ2QwiZ3i40NZSLiX_a6S0ftYCndVZjf4vlruWur4I3Z0o7CZuFrRMl2CKxyk30w.webp',
-        order: 2147483647,
-        category: '공지',
-        user: 9,
-      },
-      {
-        id: 2,
-        created_at: '2025-02-21T19:11:46.934281+09:00',
-        updated_at: '2025-02-21T19:11:46.934312+09:00',
-        title: '이벤트',
-        content:
-          '느아으아아아아아아ㅏㅇ으아아아아아아ㅏㅇ으아아아아아아ㅏㅇ으아아아아아아ㅏㅇ으아아아아아아ㅏㅇ으아아아아아아ㅏㅇ으아아아아아아ㅏㅇ으아아아아아아ㅏㅇ',
-        order: 2147483647,
-        category: '이벤트',
-        user: 9,
-      },
-    ],
-  };
-
-  const notice = data.results.find(item => item.id.toString() === noticeId);
-
-  if (!notice) {
-    return <div className="p-8">공지사항을 찾을 수 없습니다</div>;
-  }
-
   return (
-    <div className="flex justify-center items-center w-full max-w-[700px] h-full min-h-[700px] max-h-[800px] min-w-[300px]">
-      <ContentBox size="max-w-4xl " layout="gap-10 px-6 ">
+    <div className="flex justify-center items-center w-full max-w-[700px] min-w-[300px] h-full max-h-[800px]">
+      <ContentBox size="max-w-4xl h-full" layout="gap-10 px-6">
         <h3 className="font-lilita text-cream stroke text-3xl md:text-4xl">NOTICE</h3>
-        <NoticeDetail notice={notice} />
+        <NoticeDetail noticeId={noticeId} />
       </ContentBox>
     </div>
   );
 };
-
 export default NoticeDetailPage;
