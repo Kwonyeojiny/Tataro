f85d800949e23f03472b529d57ff90bba9cbf524
Sangwoo Shin <ssangwoo96@gmail.com>
2025-02-23 03:13:34 +0900

    design: 패딩 간격 수정

--- src/components/notice/noticeDetail/index.tsx
+++ src/components/notice/noticeDetail/index.tsx
@@ -12,14 +12,14 @@ type Props = {
 const NoticeDetail = ({ notice }: Props) => {
   return (
     <div className="flex flex-col items-center h-full w-full max-h-[567px] gap-3 ">
-      <div className="flex justify-start items-center gap-3 w-full h-10 p-3 border border-purple bg-cream ">
+      <div className="flex justify-start items-center gap-3 w-full h-10 px-4 py-2 border border-purple bg-cream ">
         <h3 className="text-xl text-purple font-gBold">{`[${notice.category}]`}</h3>
         <h3 className="text-xl text-purple font-gBold">{notice.title}</h3>
       </div>
       <div className="flex justify-end w-full text-sm text-gray-600 font-gMedium">
         {new Date(notice.created_at).toLocaleDateString()}
       </div>
-      <div className="flex flex-col items-center gap-10 w-full h-full p-10 border border-purple bg-cream overflow-y-auto">
+      <div className="flex flex-col items-center gap-10 w-full h-full p-4 border border-purple bg-cream overflow-y-auto">
         {notice.img_url && (
           <Image src={notice.img_url} alt={notice.title} width={200} height={200} />
         )}
