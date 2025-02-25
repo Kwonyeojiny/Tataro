f13203134a29aac36e3ed9299fe1ee4f9bd0d384
hatnim <dlgotsla3391@gmail.com>
2025-02-26 02:58:07 +0900

    fix: ReviewFormAuthGuard 컴포넌트 렌더링 오류 수정

--- src/components/reviews/reviewFormAuthGuard/index.tsx
+++ src/components/reviews/reviewFormAuthGuard/index.tsx
@@ -3,39 +3,59 @@
 import { useRouter } from 'next/navigation';
 import { useSession } from 'next-auth/react';
 
+import { ReactNode, useEffect, useState } from 'react';
+
+import LoadingSpinner from '@common/loadingSpinner';
 import { layerPopup } from '@common/layerPopup';
 
-const ReviewFormAuthGuard = ({
-  userId,
-  children,
-}: {
+type ReviewFormAuthGuardProps = {
+  children: ReactNode;
   userId: number;
-  children: React.ReactNode;
-}) => {
-  const { data: session } = useSession();
-  const user = session?.user;
+};
+const ReviewFormAuthGuard = ({ userId, children }: ReviewFormAuthGuardProps) => {
+  const { data: session, status } = useSession();
   const router = useRouter();
+  const [isAuthorized, setIsAuthorized] = useState<boolean | null>(null);
 
-  if (!user) {
-    layerPopup({
-      type: 'alert',
-      content: '로그인이 필요한 기능입니다.',
-      onConfirmClick: () => {
-        router.push('/login');
-      },
-    });
+  useEffect(() => {
+    if (status === 'loading') return;
+
+    const user = session?.user;
+
+    if (!user) {
+      layerPopup({
+        type: 'alert',
+        content: '로그인이 필요한 기능입니다.',
+        onConfirmClick: () => {
+          router.push('/login');
+        },
+      });
+      setIsAuthorized(false);
+      return;
+    }
+
+    if (user.user_id !== userId) {
+      layerPopup({
+        type: 'alert',
+        content: '리뷰 작성 권한이 없습니다.',
+        onConfirmClick: () => {
+          router.push('/');
+        },
+      });
+      setIsAuthorized(false);
+      return;
+    }
+    setIsAuthorized(true);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [session, status, userId]);
+
+  if (status === 'loading' || isAuthorized === null) {
+    return <LoadingSpinner />;
+  }
+  if (!isAuthorized) {
     return null;
   }
-  if (user.user_id !== userId) {
-    layerPopup({
-      type: 'alert',
-      content: '리뷰 작성 권한이 없습니다.',
-      onConfirmClick: () => {
-        router.push('/');
-      },
-    });
-    return null;
-  }
+
   return <>{children}</>;
 };
 
