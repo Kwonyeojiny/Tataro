6932d765bb0373892a2d2455832bedbdac0f69f0
Seyoon Jeong <seyoonagain@gmail.com>
2025-02-25 10:43:50 +0900

    refactor: 사용자 API 요청 방식 업데이트에 따라 관련 코드 업데이트

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,33 +1,31 @@
 'use client';
 
 import SocialLoginButton from '@/components/socialLoginButton';
-import useUserActions from '@/hooks/useUserActions';
+import useUserQueries from '@/hooks/useUserQueries';
 
 import ContentBox from '@common/contentBox';
-
-import { OAuthProviderType } from '@root/next-auth';
+import LoadingSpinner from '@common/loadingSpinner';
 
 import { OAUTH_PROVIDERS } from './constants';
+import { ERROR_MESSAGES } from '@/hooks/constants';
 
 const Login = () => {
-  const { redirectToSocialLogin } = useUserActions();
+  const { handleError, isLoginRedirectLoading, loginRedirectError } = useUserQueries();
 
-  const handleSocialLogin = (OAuthProvider: OAuthProviderType) => {
-    redirectToSocialLogin(OAuthProvider);
-  };
+  if (isLoginRedirectLoading) return <LoadingSpinner />;
+
+  if (loginRedirectError) {
+    handleError(loginRedirectError, ERROR_MESSAGES.GENERAL_ERROR);
+  }
 
   return (
     <div className="flex justify-center items-center w-full h-full">
-      <ContentBox size="max-w-md h-[576px]" layout="gap-10 px-6 pt-10 pb-20">
+      <ContentBox size="max-w-[420px] h-[576px]" layout="gap-10 px-6 pt-10 pb-20">
         <div className="w-full h-48 bg-logo bg-center bg-contain bg-no-repeat" aria-label="로고" />
         <p className="grow content-center font-lilita text-cream text-3xl stroke">Login</p>
         <div className="flex flex-col items-center gap-5 w-full">
           {OAUTH_PROVIDERS.map(OAuthProvider => (
-            <SocialLoginButton
-              key={OAuthProvider}
-              OAuthProvider={OAuthProvider}
-              onClick={() => handleSocialLogin(OAuthProvider)}
-            />
+            <SocialLoginButton key={OAuthProvider} OAuthProvider={OAuthProvider} />
           ))}
         </div>
       </ContentBox>
--- src/components/socialLoginButton/index.tsx
+++ src/components/socialLoginButton/index.tsx
@@ -1,5 +1,8 @@
 import Image from 'next/image';
+import { useRouter } from 'next/navigation';
 import { twMerge } from 'tailwind-merge';
+
+import useUserQueries from '@/hooks/useUserQueries';
 
 import kakao from '@images/kakao.svg';
 import naver from '@images/naver.svg';
@@ -8,6 +11,11 @@ import SocialLoginButtonProps from './types';
 import { OAUTH_PROVIDERS_KOR } from '@/app/login/constants';
 
 const SocialLoginButton: React.FC<SocialLoginButtonProps> = ({ OAuthProvider, ...props }) => {
+  const { loginRedirectUrl } = useUserQueries(OAuthProvider);
+  const router = useRouter();
+
+  const handleSocialLogin = () => loginRedirectUrl && router.push(loginRedirectUrl);
+
   return (
     <button
       type="button"
@@ -15,6 +23,7 @@ const SocialLoginButton: React.FC<SocialLoginButtonProps> = ({ OAuthProvider, ..
         'flex justify-center items-center gap-2 z-10 w-full max-w-80 h-12 px-4 rounded-xl text-lg font-medium',
         OAuthProvider === 'kakao' ? 'bg-kakao text-kakaoText' : 'bg-naver text-white',
       )}
+      onClick={handleSocialLogin}
       {...props}
     >
       <Image
